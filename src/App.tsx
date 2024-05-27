b218e644912be3c6ccedb0cce3e2a973190957ff
sebbex1337 <sefort@hotmail.dk>
2024-05-27 13:14:42 +0200

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
 export default function App() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<h1>Hej</h1>} />
+                <Route path="/about" element={<h1>Med dig</h1>} />
+            </Routes>
         </div>
     );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">About</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
